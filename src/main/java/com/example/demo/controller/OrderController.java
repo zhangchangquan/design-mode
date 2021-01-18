470fc344d7c74982d3adb5ec237ea390eadf233c
zhangchangquan <123456wn>
2021-01-18 10:50:23 +0800

    dev-0118

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,33 +1,19 @@
 package com.example.demo.controller;
 
-import com.example.demo.component.Context;
-import com.example.demo.service.State;
-import com.example.demo.service.impl.PendingPaymentState;
+import com.example.demo.model.OrderModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class OrderController {
 
+    @Autowired
+    private static OrderModel orderModel;
+
     public static void main(String[] args) {
-        State orderState = new PendingPaymentState();
-        Context context = new Context(orderState);
-        context.printInfo();
 
-        // 用户付款
-        context.action();
-        context.printInfo();
+        orderModel.updateOrderState();
 
-        // 商家出库发货
-        context.action();
-        context.printInfo();
-
-        // 用户签收
-        context.action();
-        context.printInfo();
-
-        // 用户评价
-        context.action();
-        context.printInfo();
 
     }
 }
--- src/main/java/com/example/demo/model/OrderModel.java
+++ src/main/java/com/example/demo/model/OrderModel.java
@@ -1,11 +1,32 @@
 package com.example.demo.model;
 
+import com.example.demo.component.Context;
+import com.example.demo.service.State;
+import com.example.demo.service.impl.PendingPaymentState;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderModel {
 
     public void updateOrderState() {
+        State orderState = new PendingPaymentState();
+        Context context = new Context(orderState);
+        context.printInfo();
 
+        // 用户付款
+        context.action();
+        context.printInfo();
+
+        // 商家出库发货
+        context.action();
+        context.printInfo();
+
+        // 用户签收
+        context.action();
+        context.printInfo();
+
+        // 用户评价
+        context.action();
+        context.printInfo();
     }
 }
