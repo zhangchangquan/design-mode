4cb29f6e5281d7df39e1516c161739e5eb3f4c2a
zhangchangquan <123456wn>
2021-01-18 10:54:54 +0800

    dev-0118

--- src/main/java/com/example/demo/model/OrderModel.java
+++ src/main/java/com/example/demo/model/OrderModel.java
@@ -1,11 +1,35 @@
 package com.example.demo.model;
 
+import com.example.demo.component.Context;
+import com.example.demo.service.State;
+import com.example.demo.service.impl.PendingPaymentState;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderModel {
 
     public void updateOrderState() {
-        System.out.println("0118");
+        State orderState = new PendingPaymentState();
+        Context context = new Context(orderState);
+        context.printInfo();
+
+        // 用户付款
+        context.action();
+        context.printInfo();
+
+        // 增加一种状态
+        System.out.println("增加一种状态");
+
+        // 商家出库发货
+        context.action();
+        context.printInfo();
+
+        // 用户签收
+        context.action();
+        context.printInfo();
+
+        // 用户评价
+        context.action();
+        context.printInfo();
     }
 }
