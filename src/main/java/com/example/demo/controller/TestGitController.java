2be8cbe7230f2738bff0281994edbf451765358d
zhangchangquan <123456wn>
2021-02-04 19:01:21 +0800

    main second commit

--- src/main/java/com/example/demo/controller/TestGitController.java
+++ src/main/java/com/example/demo/controller/TestGitController.java
@@ -3,13 +3,11 @@ package com.example.demo.controller;
 public class TestGitController {
 
     public static void main(String[] args) {
-        //
+        System.out.println("start");
         System.out.println("hello world");
-        //
         System.out.println("第二条记录");
-        //
         System.out.println("测试暂存区");
-        //
+        System.out.println("end");
 
     }
 }
